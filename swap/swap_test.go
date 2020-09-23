984ba06d0f4212bde074ad182fed643b4c010dec
HaoyangLiu <lhy.0318@163.com>
2020-09-23 22:40:04 +0800

    TestSwap_SwapInstanceCheck

--- swap/swap_test.go
+++ swap/swap_test.go
@@ -1,22 +1,18 @@
 package swap
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/binance-chain/bsc-eth-swap/model"
 	"github.com/ethereum/go-ethereum/ethclient"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSwap_InsertAndConfirmSwapRequest(t *testing.T) {
+func TestSwap_SwapInstanceCheck(t *testing.T) {
 	config := getTestConfig()
-
-	db, err := prepareDB(config)
-	require.NoError(t, err)
-
-	err = insertABCToken(db)
-	require.NoError(t, err)
 
 	bscClient, err := ethclient.Dial(config.ChainConfig.BSCProvider)
 	require.NoError(t, err)
@@ -24,16 +20,26 @@ func TestSwap_InsertAndConfirmSwapRequest(t *testing.T) {
 	ethClient, err := ethclient.Dial(config.ChainConfig.ETHProvider)
 	require.NoError(t, err)
 
-	swapInstance, err := NewSwapper(db, config, bscClient, ethClient)
+	db, err := prepareDB(config)
 	require.NoError(t, err)
 
-	clientPrivateKey := "580c4a31fffda2006462183b6ecc82a67a7fc772568c5b7191e3ed5e4be1bf04"
+	err = insertABCToken(db)
+	require.NoError(t, err)
 
-	{
-		go swapInstance.monitorSwapRequestDaemon()
-		go swapInstance.confirmSwapRequestDaemon()
-		go swapInstance.createSwapDaemon()
-		go swapInstance.trackSwapTxDaemon()
+	err = insertDEFToken(db)
+	require.NoError(t, err)
+
+	swapInstance, err := prepareTest(db, bscClient, ethClient, config)
+	require.NoError(t, err)
+
+	require.Equal(t, 2, len(swapInstance.TokenInstances))
+
+	tokens := make([]model.Token, 0)
+	db.Find(&tokens)
+
+	for _, token := range tokens {
+		require.Equal(t, token.Symbol, swapInstance.BSCContractAddrToSymbol[strings.ToLower(token.BSCTokenContractAddr)])
+		require.Equal(t, token.Symbol, swapInstance.ETHContractAddrToSymbol[strings.ToLower(token.ETHTokenContractAddr)])
 	}
 
 }
--- swap/test_util.go
+++ swap/test_util.go
@@ -1,13 +1,35 @@
 package swap
 
 import (
+	"crypto/ecdsa"
 	"io/ioutil"
+	"math/big"
 	"strings"
+	"time"
 
 	"github.com/binance-chain/bsc-eth-swap/model"
+	"github.com/binance-chain/bsc-eth-swap/swap/erc20"
+	"github.com/binance-chain/bsc-eth-swap/swap/swapproxy"
 	"github.com/binance-chain/bsc-eth-swap/util"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethcom "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/jinzhu/gorm"
 )
+
+func prepareTest(db *gorm.DB, bscClient, ethClient *ethclient.Client, config *util.Config) (*Swapper, error) {
+	swapInstance, err := NewSwapper(db, config, bscClient, ethClient)
+	if err != nil {
+		return nil, err
+	}
+	{
+		go swapInstance.monitorSwapRequestDaemon()
+		go swapInstance.confirmSwapRequestDaemon()
+		go swapInstance.createSwapDaemon()
+		go swapInstance.trackSwapTxDaemon()
+	}
+	return swapInstance, nil
+}
 
 func getTestConfig() *util.Config {
 	config := util.ParseConfigFromFile("../config/config.json")
@@ -29,6 +51,14 @@ func prepareDB(config *util.Config) (*gorm.DB, error) {
 	return db, nil
 }
 
+func getClientAccount() (*ecdsa.PrivateKey, ethcom.Address, error) {
+	privateKey, publicKey, err := BuildKeys("580c4a31fffda2006462183b6ecc82a67a7fc772568c5b7191e3ed5e4be1bf04")
+	if err != nil {
+		return nil, ethcom.Address{}, err
+	}
+	return privateKey, GetAddress(publicKey), nil
+}
+
 func insertABCToken(db *gorm.DB) error {
 	token := model.Token{
 		Symbol:               "ABC",
@@ -37,12 +67,12 @@ func insertABCToken(db *gorm.DB) error {
 		BSCTokenContractAddr: strings.ToLower("0xCCE0532FE1029f1A6B7ccca4C522cF9870a6a8Ed"),
 		ETHTokenContractAddr: strings.ToLower("0x055d208b90DA0E3A431CA7E0fba326888Ef8a822"),
 		LowBound:             "0",
-		UpperBound:          "1000000000000000000000000",
-		BSCSenderAddr:       "0x0C5006c9322b6dC49BC475d7635659F7147326d3",
-		BSCERC20Threshold:   "1000000000000000000",
-		ETHSenderAddr:       "0x0C5006c9322b6dC49BC475d7635659F7147326d3",
-		ETHERC20Threshold:   "1000000000000000000",
-		Available:           true,
+		UpperBound:           "1000000000000000000000000",
+		BSCSenderAddr:        "0x0C5006c9322b6dC49BC475d7635659F7147326d3",
+		BSCERC20Threshold:    "1000000000000000000",
+		ETHSenderAddr:        "0x0C5006c9322b6dC49BC475d7635659F7147326d3",
+		ETHERC20Threshold:    "1000000000000000000",
+		Available:            true,
 	}
 
 	tx := db.Begin()
@@ -65,12 +95,12 @@ func insertDEFToken(db *gorm.DB) error {
 		BSCTokenContractAddr: strings.ToLower("0x8f36F4A709409a95a0df90cbc43ED9a658E11E4A"),
 		ETHTokenContractAddr: strings.ToLower("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"),
 		LowBound:             "0",
-		UpperBound:          "1000000000000000000000000",
-		BSCSenderAddr:       "0xb0438919ABefa48a43635279d822735d31b5d762",
-		BSCERC20Threshold:   "1000000000000000000",
-		ETHSenderAddr:       "0xb0438919ABefa48a43635279d822735d31b5d762",
-		ETHERC20Threshold:   "1000000000000000000",
-		Available:           true,
+		UpperBound:           "1000000000000000000000000",
+		BSCSenderAddr:        "0xb0438919ABefa48a43635279d822735d31b5d762",
+		BSCERC20Threshold:    "1000000000000000000",
+		ETHSenderAddr:        "0xb0438919ABefa48a43635279d822735d31b5d762",
+		ETHERC20Threshold:    "1000000000000000000",
+		Available:            true,
 	}
 
 	tx := db.Begin()
@@ -83,4 +113,47 @@ func insertDEFToken(db *gorm.DB) error {
 		return err
 	}
 	return tx.Commit().Error
+}
+
+func buildTransactor(privateKey *ecdsa.PrivateKey, value *big.Int) *bind.TransactOpts {
+	txOpt := bind.NewKeyedTransactor(privateKey)
+	txOpt.Value = value
+	return txOpt
+}
+
+func swapBSC2ETH(client *ethclient.Client, privateKey *ecdsa.PrivateKey, bscSwapProxyContractAddr, tokenAddr ethcom.Address, amount *big.Int) (ethcom.Hash, error) {
+	bscERC20Instance, _ := erc20.NewErc20(tokenAddr, client)
+	bscSwapProxy, _ := swapproxy.NewSwap(bscSwapProxyContractAddr, client)
+
+	approveTx, err := bscERC20Instance.Approve(buildTransactor(privateKey, nil), bscSwapProxyContractAddr, amount)
+	if err != nil {
+		return ethcom.Hash{}, err
+	}
+	util.Logger.Infof("approveTx Hash: https://testnet.bscscan.com/tx/%s", approveTx.Hash().String())
+	time.Sleep(500 * time.Millisecond)
+
+	swapTx, err := bscSwapProxy.Swap(buildTransactor(privateKey, big.NewInt(100000)), tokenAddr, amount)
+	if err != nil {
+		return ethcom.Hash{}, err
+	}
+	util.Logger.Infof("https://testnet.bscscan.com/tx/%s", swapTx.Hash().String())
+	return swapTx.Hash(), nil
+}
+
+func swapETH2BSC(client *ethclient.Client, privateKey *ecdsa.PrivateKey, ethSwapProxyContractAddr, tokenAddr ethcom.Address, amount *big.Int) (ethcom.Hash, error) {
+	ethERC20Instance, _ := erc20.NewErc20(tokenAddr, client)
+	ethSwapProxy, _ := swapproxy.NewSwap(ethSwapProxyContractAddr, client)
+	approveTx, err := ethERC20Instance.Approve(buildTransactor(privateKey, nil), ethSwapProxyContractAddr, amount)
+	if err != nil {
+		return ethcom.Hash{}, err
+	}
+	util.Logger.Infof("approveTx Hash: https://rinkeby.etherscan.io/tx/%s", approveTx.Hash().String())
+	time.Sleep(500 * time.Millisecond)
+
+	swapTx, err := ethSwapProxy.Swap(buildTransactor(privateKey, big.NewInt(100000)), tokenAddr, amount)
+	if err != nil {
+		return ethcom.Hash{}, err
+	}
+	util.Logger.Infof("https://rinkeby.etherscan.io/tx/%s", swapTx.Hash().String())
+	return swapTx.Hash(), nil
 }
